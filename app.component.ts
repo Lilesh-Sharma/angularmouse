efce8e8648b3cbde2c4c397519e84fe0da106a92
Lilesh-Sharma <62554423+Lilesh-Sharma@users.noreply.github.com>
2020-03-29 23:24:53 +0530

    commit

---
+++ app.component.ts
@@ -0,0 +1,24 @@
+import { Component ,HostListener} from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+   data:string="";
+  onMouseMove(event: MouseEvent) {
+   this.data = "Lilesh";
+  }
+
+    public text: String = 'Hello.';
+  public test=true;
+
+
+  
+
+  mouseMove() {
+    this.test = false;
+    
+  }
+}
